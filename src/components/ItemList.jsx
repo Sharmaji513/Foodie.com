f3ae249d9d87913fa9666178b79f1d2d482c744e
sachin <sk800968@gmail.com>
2024-08-25 00:48:54 +0530

    added Accordion feature in Restaurent Menu

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,48 +1,57 @@
-import React from 'react'
-import { CDN_URL } from '../utils/constents';
+import React from "react";
+import { CDN_URL } from "../utils/constents";
 
-const ItemList = ({items}) => {
-
-    
-    console.log(items);
-    return (
-        <div>
-        {items.map((item)=>(
-            <>
-           
-           <div className="p-2 m-2 border-gray-200 text-left flex"
-          key={item.card.info.id}
-        >
-          <div className="w-9/12">
-            <span className='text-[#02060C] font-bold'>{item.card.info.name}</span>
-            <div>
-              <span> ₹{item.card.info.price / 100 || item.card.info.defaultPrice/100} <span className='text-[12px] font-  text-[#02060C] font-bold tracking-wide leading-3 '>🏷️40% OFF USE SWIGGYIT</span></span>
+const ItemList = ({ items }) => {
+  console.log(items);
+  return (
+    <div>
+      {items.map((item) => (
+        <>
+          <div
+            className="p-2 m-2 border-gray-200 text-left flex"
+            key={item.card.info.id}
+          >
+            <div className="w-9/12">
+              <span className="text-[#02060C] font-bold">
+                {item.card.info.name}
+              </span>
+              <div>
+                <span>
+                  {" "}
+                  ₹
+                  {item.card.info.price / 100 ||
+                    item.card.info.defaultPrice / 100}{" "}
+                  <span className="text-[12px] font-  text-[#02060C] font-bold tracking-wide leading-3 ">
+                    🏷️40% OFF USE SWIGGYIT
+                  </span>
+                </span>
+              </div>
+              <p className="text-xs my-3 font-[#02060C]">
+                {item.card.info.description}
+              </p>
             </div>
-            <p className="text-xs my-3 font-[#02060C]">{item.card.info.description}</p>
-          </div>
 
-          <div className="w-3/12 p-4">
-            <div className="absolute">
-              <button
-                onClick={() => handleAddItem(item)}
-                className="p-2 ml-8 mt-[110px] rounded-lg bg-white text-green-600 font-bold shadow-lg hover:bg-white  hover:text-black transition-all duration-[.3s]"
-              >
-                Add +
-              </button>
+            <div className="w-3/12 p-4">
+              <div className="absolute">
+                <button
+                  onClick={() => handleAddItem(item)}
+                  className="p-2 ml-8 mt-[110px] rounded-lg bg-white text-green-600 font-bold shadow-lg hover:bg-white  hover:text-black transition-all duration-[.3s]"
+                >
+                  Add +
+                </button>
+              </div>
+
+              <img
+                src={CDN_URL + item.card.info.imageId}
+                alt={item.card.info.name}
+                className="w-full rounded-md"
+              />
             </div>
-
-            <img
-              src={CDN_URL + item.card.info.imageId}
-              alt={item.card.info.name}
-              className="w-full rounded-md"
-            />
           </div>
-        </div>
- 
-           </>
-        ))}
+        </>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ItemList
+export default ItemList;
--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -2,26 +2,31 @@ import React, { useState } from "react";
 import ItemList from "./ItemList";
 
 const RestaurantCategory = ({ data }) => {
-//   console.log(data);
+  //console.log(data);
 
-//   const [showItems , setShowItem] = useState(true)
+  const [showItems, setShowItem] = useState(false);
 
-//   const handleClick = () => {
-//     // console.log("clicked");
-//     setShowItem(!showItems)
-//   };
+  const handleClick = () => {
+    // console.log("clicked");
+    setShowItem(!showItems);
+  };
+
   return (
     <>
-    
-      <div className=" w-6/12 bg-gray-50 mx-auto shadow-lg my-4  p-4  ">
-        <div className="flex justify-between cursor-pointer">
+      <div className=" w-6/12 bg-gray-50 mx-auto shadow-lg my-4  p-4 ">
+        <div
+          onClick={handleClick}
+          className="flex justify-between cursor-pointer"
+        >
           {/*Accordion Header */}
-          <span   className="font-bold text-lg">
+          <span className="font-bold text-lg">
             {data.title} ({data.itemCards.length})
           </span>
-          <span>🠿</span>
+          <span>▼</span>
         </div>
-        <div> <ItemList items={data.itemCards}/></div>
+
+         {/*Accordion Items */}
+        {showItems && <ItemList items={data.itemCards} />}
       </div>
     </>
   );
