2ea4738633def2e1467821b9ac9892deb5637c06
sachin <sk800968@gmail.com>
2024-08-26 21:22:18 +0530

    added cartSlice

---
+++ src/store/cartSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+
+  initialState: {
+    items: [],
+  },
+
+  reducers: {
+    addItems: (state, action) => {
+      state.items.push(action.payload);
+    },
+
+    removeItem: (state, action) => {
+      state.items.pop();
+    },
+    clearCart: (state, actions) => {
+      state.items.length = 0;   // []
+    },
+  },
+});
+
+
+export const {addItems , removeItem ,clearCart} = cartSlice.actions;
+
+export default cartSlice.reducer;
