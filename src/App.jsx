4683b3c00ce8cefa25019ea7be285928d033e4f4
sachin <sk800968@gmail.com>
2024-08-26 21:06:43 +0530

    setup Redux Toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,20 @@ import "./App.css";
 
 import Body from "./components/Body";
 import RestaurantMenu from "./components/RestaurantMenu";
-
+import { Provider } from "react-redux";
+import appStore from "./store/appStore";
 
 function App() {
   return (
     <>
-
-    <div className="app">
-
-      <Routes>
-      
-    
-      <Route path="/" element={<Body />} />
-      <Route path="/restaurants/:resId" element={<RestaurantMenu/>} />
-        
-      </Routes>
-
-    </div>
-    
+      <Provider store={appStore}>
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<Body />} />
+            <Route path="/restaurants/:resId" element={<RestaurantMenu />} />
+          </Routes>
+        </div>
+      </Provider>
     </>
   );
 }
